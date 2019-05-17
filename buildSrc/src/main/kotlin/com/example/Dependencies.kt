579e3c90dbcad03ca5f5aace174ad98520a276e7
Aurimas Liutikas <aurimas@liutikas.net>
2019-05-16 19:04:01 -0700

    Add dependencies file

---
+++ buildSrc/src/main/kotlin/com/example/Dependencies.kt
@@ -0,0 +1,12 @@
+package com.example
+
+const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:1.0.2"
+const val ANDROIDX_CORE = "androidx.core:core-ktx:1.0.2"
+const val ANDROIDX_CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+const val ANDROIDX_TEST_RUNNER = "androidx.test:runner:1.1.1"
+const val ANDROIDX_TEST_ESPRESSO = "androidx.test.espresso:espresso-core:3.1.1"
+
+const val JUNIT = "junit:junit:4.12"
+
+const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:1.3.31"
