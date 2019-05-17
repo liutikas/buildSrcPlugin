51a1ac2b772f4f666141caf91e1333df9d4c626f
Aurimas Liutikas <aurimas@liutikas.net>
2019-05-17 14:12:04 -0700

    Fun fun hackery to make minSdkVersion setting work.

--- buildSrc/src/main/kotlin/com/example/MyPlugin.kt
+++ buildSrc/src/main/kotlin/com/example/MyPlugin.kt
@@ -9,10 +9,15 @@ class MyPlugin : Plugin<Project> {
         val myConfig = project.extensions.create("myConfig",
                 MyPluginExtension::class.java)
 
+        // Fun fun hackery time... Android Gradle Plugin ignores changes to defaultConfig after
+        // it runs its afterEvaluate blocks. We need to read the value from our extension which
+        // is only populated by afterEvaluate, so... we need to run our change in an afterEvaluate
+        // before Android Gradle Plugin's after evaluate. To summarize, we rely on the following:
+        // - MyPlugin is applied before Android Gradle Plugin (in each build.gradle)
+        // - afterEvaluate below runs before Android Gradle Plugin afterEvaluates.
         project.afterEvaluate {
-            project.extensions.findByType(LibraryExtension::class.java)?.apply {
+            project.extensions.findByType(TestedExtension::class.java)?.apply {
                 defaultConfig.minSdkVersion(myConfig.minSdkVersion.get())
-                println("aurimas minSdkVersion ${myConfig.minSdkVersion.get()}")
             }
         }
 
@@ -20,12 +25,12 @@ class MyPlugin : Plugin<Project> {
             when (plugin) {
                 is LibraryPlugin -> {
                     project.extensions.getByType(LibraryExtension::class.java).apply {
-                        setCommonAndroidOptions(project, myConfig)
+                        setCommonAndroidOptions()
                     }
                 }
                 is AppPlugin -> {
                     project.extensions.getByType(AppExtension::class.java).apply {
-                        setCommonAndroidOptions(project, myConfig)
+                        setCommonAndroidOptions()
                         defaultConfig.apply {
                             versionCode = 1
                             versionName = "1.0"
@@ -43,20 +48,12 @@ class MyPlugin : Plugin<Project> {
         }
     }
 
-    private fun TestedExtension.setCommonAndroidOptions(
-            project: Project,
-            myConfig: MyPluginExtension
-    ) {
+    private fun TestedExtension.setCommonAndroidOptions() {
         compileSdkVersion(28)
         defaultConfig.apply {
-            //minSdkVersion(24)
+            minSdkVersion(24)
             targetSdkVersion(28)
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        }
-
-        project.afterEvaluate {
-            defaultConfig.minSdkVersion(myConfig.minSdkVersion.get())
-            println("aurimas minSdkVersion ${myConfig.minSdkVersion.get()}")
         }
     }
 }
